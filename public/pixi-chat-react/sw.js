2aab079d3df9294e8fe6904643c4b07698589f55
Abdul Mannan <abdulmmm556@gmail.com>
2026-01-02 18:57:35 +0500

    Add service worker with install, activate, and fetch events

---
+++ public/pixi-chat-react/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetching:', event.request.url);
+});
