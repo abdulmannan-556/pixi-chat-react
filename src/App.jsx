acfa66d9019059a32b40023a79e14d09151aef57
Abdul Mannan <abdulmmm556@gmail.com>
2026-01-02 18:59:13 +0500

    Create App component with ChatCanvas integration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatCanvas from "./components/ChatCanvas";
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-gray-100">
+      <ChatCanvas />
+    </div>
+  );
+}
+
+export default App;
